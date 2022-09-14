73c9aabe0742fc03e8328b1d0847fbc46b0a7588
Alissonred <108753902+Alissonred@users.noreply.github.com>
2022-09-14 07:10:50 -0500

    test y firebase

---
+++ src/firebase/init.js
@@ -0,0 +1,32 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js";
+import config from './config.js';
+
+const app = initializeApp(config);
+
+const auth = getAuth(app);
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+
+// Detectando el estado de autenticación
+onAuthStateChanged(auth, user => {
+  if (user != null) {
+    console.log('Usuario Conectado');
+  } else {
+    console.log('No se encuentra el usuario')
+  }
+});
+
+///////////////////////////////////////////////////////
+
+export {initializeApp, getAuth, createUserWithEmailAndPassword, auth}
